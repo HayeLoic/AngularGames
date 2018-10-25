70887c66111a302633fb920a0a8ef28b49711efb
loic.haye@ipsos.com <loic.haye@ipsos.com>
2018-10-25 17:56:19 +0200

    display the grid

--- angular-games/src/app/tic-tac-toe/tic-tac-toe.component.ts
+++ angular-games/src/app/tic-tac-toe/tic-tac-toe.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Square } from './square';
 
 @Component({
   selector: 'app-tic-tac-toe',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tic-tac-toe.component.css']
 })
 export class TicTacToeComponent implements OnInit {
+  squares: Square[] = [];
+  maxSquareCount: Number = 9;
 
   constructor() { }
 
   ngOnInit() {
+    for (let squareCount = 1; squareCount <= this.maxSquareCount; squareCount++) {
+      this.squares.push(new Square(squareCount, ''));
+    }
   }
-
 }
