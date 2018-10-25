52dddcc0d6a2ab828370ebe8efdd114ecf7254c1
loic.haye@ipsos.com <loic.haye@ipsos.com>
2018-10-25 16:57:50 +0200

    cleaning

--- angular-games/src/app/app-routing.module.ts
+++ angular-games/src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TicTacToeComponent } from './tic-tac-toe/tic-tac-toe.component';
-import { OtherComponent } from './other/other.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/tic-tac-toe', pathMatch: 'full' },
-  { path: 'tic-tac-toe', component: TicTacToeComponent },
-  { path: 'other', component: OtherComponent }
+  { path: 'tic-tac-toe', component: TicTacToeComponent }
 ];
 
 @NgModule({
--- angular-games/src/app/app.module.ts
+++ angular-games/src/app/app.module.ts
@@ -4,13 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { TicTacToeComponent } from './tic-tac-toe/tic-tac-toe.component';
-import { OtherComponent } from './other/other.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TicTacToeComponent,
-    OtherComponent
+    TicTacToeComponent
   ],
   imports: [
     BrowserModule,
