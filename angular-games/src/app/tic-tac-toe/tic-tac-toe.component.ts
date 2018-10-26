cfefd67061c319f41ab6e52e69f8f6a5a3f39016
loic.haye@ipsos.com <loic.haye@ipsos.com>
2018-10-26 10:37:03 +0200

    draw X/O when click

--- angular-games/src/app/tic-tac-toe/tic-tac-toe.component.ts
+++ angular-games/src/app/tic-tac-toe/tic-tac-toe.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Square } from './square';
+import { Player } from './player';
 
 @Component({
   selector: 'app-tic-tac-toe',
@@ -8,13 +9,52 @@ import { Square } from './square';
 })
 export class TicTacToeComponent implements OnInit {
   squares: Square[] = [];
+  players: Player[] = [];
   maxSquareCount: Number = 9;
+  currentPlayer: Player = null;
 
   constructor() { }
 
   ngOnInit() {
-    for (let squareCount = 1; squareCount <= this.maxSquareCount; squareCount++) {
-      this.squares.push(new Square(squareCount, ''));
+    this.squares = this.initializeSquares(this.maxSquareCount);
+    this.players = this.initializePlayers();
+    this.currentPlayer = this.players[0];
+  }
+
+  initializeSquares(maxSquareCount: Number): Square[] {
+    let squares: Square[] = [];
+    for (let squareCount = 1; squareCount <= maxSquareCount; squareCount++) {
+      squares.push(new Square(squareCount, ''));
+    }
+    return squares;
+  }
+
+  initializePlayers(): Player[] {
+    let players: Player[] = [];
+    players.push(new Player(1, 'X'));
+    players.push(new Player(2, 'O'));
+    return players;
+  }
+
+  squareClick(square: Square): void {
+    if (!square.value) {
+      square = this.updateSquareValue(square, this.currentPlayer);
+      this.currentPlayer = this.updateCurrentPlayer(this.currentPlayer, this.players);
+    }
+  }
+
+  updateSquareValue(square: Square, currentPlayer: Player): Square {
+    square.value = currentPlayer.symbol;
+    return square;
+  }
+
+  updateCurrentPlayer(currentPlayer: Player, players: Player[]): Player {
+    if (currentPlayer == players[0]) {
+      return players[1];
+    }
+
+    else {
+      return players[0];
     }
   }
 }
