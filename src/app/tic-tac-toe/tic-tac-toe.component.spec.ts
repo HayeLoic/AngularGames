e88fd3b142707a8e223c8773b641d4a0ad6dd21d
loic.haye@ipsos.com <loic.haye@ipsos.com>
2018-11-06 11:49:14 +0100

    unit test fix

--- src/app/tic-tac-toe/tic-tac-toe.component.spec.ts
+++ src/app/tic-tac-toe/tic-tac-toe.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 
 import { TicTacToeComponent } from './tic-tac-toe.component';
 
@@ -8,7 +9,8 @@ describe('TicTacToeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TicTacToeComponent ]
+      declarations: [ TicTacToeComponent ],
+      imports: [FormsModule]
     })
     .compileComponents();
   }));
